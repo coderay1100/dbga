7975e5254eed92ef42e60d52614df81bb7ccb655
Ray <rayp1100@gmail.com>
2013-12-06 14:38:34 +0700

    test

--- owner.php
+++ owner.php
@@ -14,7 +14,21 @@ require_once "functions/authorization.php";
 	<body>
 		<?php include "includes/navbar2.php"; ?>
 		
-		
+		<div class="container">
+			<div class="row clearfix">
+				<div class="col-md-12 column">
+					
+					<!--Page header-->
+					<div class="page-header">
+						<h1>We're the boss here! <small>Let's recruit people!</small></h1>
+					</div> <!--/page-header-->
+					
+					
+					
+					
+				</div>
+			</div>
+		</div> <!--/.container-->
 		
 		<script src="js/bootstrap.min.js"></script>
 		<script src="js/ajax.js"></script>
