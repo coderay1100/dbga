73f6e19544fd3b3b340e596598140934a72ee2f5
Ray Putra <rayp1100@gmail.com>
2013-12-08 21:19:23 +0700

    FINALLY v2

--- includes/head.php
+++ includes/head.php
@@ -1,4 +1,9 @@
 		<meta charset="utf-8">
-		<title>TITLE</title>
+		<title>Career Hub (BETA)</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
 		<script src="js/jquery.js"></script>
+		<style>
+		body {
+			padding-bottom: 100px;
+		}
+		</style>
--- viewcv.php
+++ viewcv.php
@@ -36,210 +36,195 @@ $data['name'] = $_GET['fname'] . " " . $_GET['lname'];
 		<?php include "includes/navbar2.php"; ?>
 		
 		<div class="container">
-	<div class="row clearfix">
-		<div class="col-md-12 column">
-			<div class="page-header">
-				<h1>
-					Curriculum Vitae
-				</h1>
-				
-			</div>
-			
-			<div class="page-header">
-				<h1>
-					<small>Personal Information</small>
-				</h1>
-			</div>
-			
-			<div class="row clearfix">
-				<div class="col-md-9 column">
-				<label for="companyname" class="col-sm-2 control-label"> Name: </label>
-					<div class="col-sm-3">
-						<label for="companyname" class="col-sm-2 control-label">  <?php echo $data['name']; ?></label>
-					</div>
-				</div>
-				
-				<div class="col-md-9 column">
-				<label for="companyname" class="col-sm-2 control-label"> Applicant's ID: </label>
-					<div class="col-sm-3">
-						<label for="companyname" class="col-sm-2 control-label">  <?php echo $_GET['id']; ?></label>
-					</div>
-				</div>
-			</div>
-			
-			<div class="page-header">
-				<h1>
-					<small>Job Experience</small>
-				</h1>
-			</div>
-			<div class="row clearfix">
-				<div class="col-md-9 column">						
-					<form class="form-horizontal" role="form">
-						<div class="form-group">
-							 <label for="companyname" class="col-sm-2 control-label"> Company Name: </label>
-							<div class="col-sm-3">
-								<label for="companyname" class="col-sm-2 control-label">  <?php echo $data['company']; ?> </label>
-							</div>
-						</div>	
-						<div class="form-group">
-							 <label for="position" class="col-sm-2 control-label">Position:</label>
-							<div class="col-sm-3">
-								<div class="col-sm-3">
-								<label for="companyname" class="col-sm-2 control-label"> <?php echo $data['position']; ?> </label>
-							</div>
-							</div>
-						</div>
-						<div class="form-group">
-							 <label for="location" class="col-sm-2 control-label">Location:</label>
-							<div class="col-sm-3">
-								<div class="col-sm-3">
-								<label for="companyname" class="col-sm-2 control-label"> <?php echo $data['location']; ?></label>
-							</div>
-							</div>
-						</div>
-					</form>
-					<div class="row clearfix">
-						<div class="col-md-6 column">
-							<form role="form">
-								<div class="form-group">
-								<br>
-							<p><label for="Startdate" type="date">Start Date (YYYY-MM-DD):</label><div class="col-sm-3"></p>		
-								<label for="companyname" class="col-sm-2 control-label"><?php echo $data['start_date']; ?></label>
-							</div>
-								</div>
-							</form>
-						</div>
-						<div class="col-md-6 column">
-							<form role="form-horizontal">
-								<div class="form-group">
-								<br>
-									 <label for="enddate" type="date">End Date (YYYY-MM-DD):</label>
-									 <br><br>
-									 <label for="companyname" class="col-sm-2 control-label">3000/12/12</label>
-								</div>
-							</form>
-						</div>
-						
+		
+			<div class="row">
+				<!-- Page header -->
 				<div class="page-header">
-				<br>
-						<br>
-				<h1>
-					<small>Honor</small>
-				</h1>
-			</div>
-			<div class="row clearfix">
-				<div class="col-md-9 column">						
-					<form class="form-horizontal" role="form">
-						<div class="form-group">
-							 <label for="award" class="col-sm-2 control-label"> Honor: </label>
-							<div class="col-sm-7">
-								<label for="companyname" class="col-sm-2 control-label">blablabla</label>
-							</div>
-						</div>	
-					</form>
-					
-				</div>
-				</div>
-				<div class="page-header">
-				<h1>
-					<small>Award(s)</small>
-				</h1>
+					<h1>Curriculum Vitae</h1>
+				</div><!--/page-header-->
 			</div>
 			
-			<div class="row clearfix">
-				<div class="col-md-9 column">						
-					<p>
-						List of all applicant's awards seperated by a comma (,) :
-					</p>
-					<div id="cid_4" class="form-input">
-						
-						<p><label for="companyname" class="col-sm-2 control-label">blablablablabla, blabla</label>
-					</p>
-					</div>					
-				</div>
-				
-				</div>
-				<br>
+			<div class="row">
+				<!-- Page header -->
 				<div class="page-header">
-				<h1>
-					<small>Education Background</small>
-				</h1>
+					<h1><small>Personal Information</small></h1>
+				</div><!--/page-header-->
 			</div>
 			
-			<div class="row clearfix">
-						<div class="col-md-6 column">
-							<form role="form">
-								<div class="form-group">
-									 <label>Field of Study:</label>
-									 <br>
-									 
-									 <label for="companyname" class="col-sm-2 control-label">ComputerScience</label>
-								</div>
-								<br>
-								<div class="form-group">
-									 <label>GPA:</label>
-									 <br>
-									 
-									 <label for="companyname" class="col-sm-2 control-label">5.0</label>
-								</div>
-							</form>
-						</div>
-						<div class="col-md-6 column">
-							<form role="form">
-								<div class="form-group">
-									 <label>Starting Year:</label>
-									 <br>
-									 <label for="companyname" class="col-sm-2 control-label">3000/12/12</label>
-								</div>
-								<br>
-								<div class="form-group">
-									 <label>Ending Year:</label>
-									 <br>
-									 <label for="companyname" class="col-sm-2 control-label">3000/12/12</label>
-								</div>
-							</form>
-						</div>
-						
-						<div class="col-md-9 column">
-							<br>						
-							<label>Activity</label>
-							<p>
-								List of all applicant's activities and organizational participation seperated by a comma (,) :
-							</p>
-							<div id="cid_4" class="form-input">
-								<label for="companyname" class="col-sm-2 control-label">blablablabla,blablabla,blablabla</label>
-							</div>					
-						</div>
-					<div class="col-md-9 column">
-						<div class="row clearfix">					
-							<form class="form-horizontal" role="form">
-							<div class="form-group"><br>
-								 <label for="degree" class="col-sm-3 control-label"> Degree: </label>
-								<div class="col-sm-7">
-									<label for="companyname" class="col-sm-2 control-label">Doctor</label>
-								</div>
-							</div>
-							
-							<form class="form-horizontal" role="form">
-							<div class="form-group">
-								 <label for="school" class="col-sm-3 control-label"> School: </label>
-								<div class="col-sm-7">
-								
-									<label for="companyname" class="col-sm-2 control-label"><p>University of Indonesia</p></label>
-								</div>
-							</div>	
-						</div>
-					
-					<br>
-				<button type="button" class="btn btn-default"> <a href="viewapp.html"> <-- List of Applicants </a></button>
-				
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Name:</strong>
 				</div>
-				<div class="col-md-3 column">
+				<div class="col-md-2">
+					<?php echo $data['name']; ?>
 				</div>
 			</div>
-		</div>
-	</div>
-</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Applicant ID:</strong>
+				</div>
+				<div class="col-md-2">
+					#<?php echo $_GET['id']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<!-- Page header -->
+				<div class="page-header">
+					<h1><small>Job Experience</small></h1>
+				</div><!--/page-header-->
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Company Name:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['company']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Position:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['position']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Location:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['location']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Start Date:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['start_date']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>End Date:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['end_date']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<!-- Page header -->
+				<div class="page-header">
+					<h1><small>Honor</small></h1>
+				</div><!--/page-header-->
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Honor:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['honor']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<!-- Page header -->
+				<div class="page-header">
+					<h1><small>Award</small></h1>
+				</div><!--/page-header-->
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Award:</strong>
+				</div>
+				<div class="col-md-10">
+					<?php echo $awd; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<!-- Page header -->
+				<div class="page-header">
+					<h1><small>Education Background</small></h1>
+				</div><!--/page-header-->
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Field of Study:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['study_field']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Starting Year:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['start_year']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Ending Year:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['end_year']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>GPA:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['gpa']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Activity:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $act; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>Degree:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['degree']; ?>
+				</div>
+			</div>
+			
+			<div class="row">
+				<div class="col-md-2">
+					<strong>School:</strong>
+				</div>
+				<div class="col-md-2">
+					<?php echo $data['school']; ?>
+				</div>
+			</div>
+			
+		</div> <!--/.container-->
+		
 	<body>
 	
 </html>
